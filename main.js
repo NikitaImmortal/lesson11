e074ea02944f9862bf1b09a4fd854de8f309e687
NikitaImmortal <nikita.kubqshko@gmail.com>
2023-01-31 18:18:39 +0200

    greate cheap lesson11

---
+++ main.js
@@ -0,0 +1,94 @@
+var a = 3;
+var b = 'Moscow' 
+var c = true;
+var d = null;
+var e;
+var f = {name: 'Ivan' }
+
+ 
+var name = 'Nikita'
+var lastName = 'Kubysko'
+var age = 31
+var maritalStatus = false
+var hasChildren = false
+
+
+console.log(typeof b);
+console.log(typeof b);
+console.log(typeof a);
+console.log(typeof c);
+console.log(typeof c);
+
+
+var lengthCM = 23;
+var lengthCM = 10;
+var result = 23 * 10
+console.log(result)
+
+var lengthM = 10
+var lenghtM = 4
+var result = 4 * 10
+console.log(result)
+
+Infinity - "1" // Infinity
+"42" + 42 // 84
+2 + "1 1" // 13
+99 + 101 // 200
+"1" - "1" // 0
+"Result: " + 10/2 // 5
+3 + " bananas " + 2 + " apples " // 5 bananas apples
+
+
+var a = Infinity
+var b = "1"
+var result = Infinity + "1"
+console.log(result)
+
+var a = "42"
+var b = 42
+var result = "42" + 42
+console.log(result)
+
+var a = 2
+var b = "1 1"
+var result = 2 + "1 1"
+console.log(result)
+
+var a = 99
+var b = 101
+var result = 99 + 101
+console.log(result)
+
+var a = "1"
+var b = "1"
+var result = "1"+ "1"
+console.log(result)
+
+var a = "Result: "
+var b = 10/2
+var result = "Result: "+ 10/2
+console.log(result)
+
+var a = 3
+var b = " bananas "
+var c = 2
+var e = " apples " 
+var result = 3 + " bananas " + 2 + " apples "
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
